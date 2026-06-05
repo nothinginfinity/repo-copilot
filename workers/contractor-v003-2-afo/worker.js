a7adc6742c2832859cde5d8b80316639f241a754
Jared Edwards <84925852+nothinginfinity@users.noreply.github.com>
2026-06-05 14:31:20 -0700

    Add contractor v003-2 worker scaffold

---
+++ workers/contractor-v003-2-afo/worker.js
@@ -0,0 +1,11 @@
+const VERSION = '0.1.0-phase1';
+const WORKER_NAME = 'contractor-v003-2-afo';
+const COMPANY_FALLBACK = 'CCS Services Group';
+const PHONE_FALLBACK = '(818) 624-7212';
+const PHONE_TEL_FALLBACK = '18186247212';
+const EMBEDDING_MODEL = '@cf/baai/bge-base-en-v1.5';
+const CHAT_MODEL = '@cf/meta/llama-3.1-8b-instruct';
+const R2_UPLOAD_PREFIX = 'contractor-v003-2/uploads/';
+const R2_MEDIA_PREFIX = 'contractor-v003-2/media/';
+
+function
