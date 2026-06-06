4046b24592b0c930ecc87cfd3d1004794f4f08ac
Jared Edwards <84925852+nothinginfinity@users.noreply.github.com>
2026-06-05 23:21:55 -0700

    Delete workers/contractor-v003-2-afo/worker.js
    Replaced this stub with complete file from cloudflare.

--- workers/contractor-v003-2-afo/worker.js
+++
@@ -1,8 +0,0 @@
-const MODEL='@cf/baai/bge-base-en-v1.5';
-const CHAT_MODEL='@cf/meta/llama-3.1-8b-instruct';
-const WORKER='contractor-v003-2-afo';
-const R2_PREFIX='contractor-v003-2/';
-function j(x,s){return new Response(JSON.stringify(x,null,2),{status:s||200,headers:{'content-type':'application/json;charset=utf-8','access-control-allow-origin':'*'}})}
-function h(x,s){return new Response(x,{status:s||200,headers:{'content-type':'text/html;charset=utf-8'}})}
-async function body(req){try{return await req.json()}catch(e){return {}}}
-function
